fd51bc783474c0c56d2e7332d15af44f2383a8ce
随便改 <81851196+Syronl-666@users.noreply.github.com>
2026-08-16 08:00:44 +0000

    多模态Embedding,把不同类型的数据类型映射到同一个共享的向量空间

--- code/C3/01_bge_visualized.py
+++ code/C3/01_bge_visualized.py
@@ -3,14 +3,17 @@ from visual_bge.visual_bge.modeling import Visualized_BGE
 
 model = Visualized_BGE(model_name_bge="BAAI/bge-base-en-v1.5",
                       model_weight="../../models/bge/Visualized_base_en_v1.5.pth")
-model.eval()
+model.eval() # 切换开关,评估模式
+
+# text="logo of organization datawhale"
+text="logo of a whale"
 
 with torch.no_grad():
-    text_emb = model.encode(text="datawhale开源组织的logo")
+    text_emb = model.encode(text=text)
     img_emb_1 = model.encode(image="../../data/C3/imgs/datawhale01.png")
-    multi_emb_1 = model.encode(image="../../data/C3/imgs/datawhale01.png", text="datawhale开源组织的logo")
+    multi_emb_1 = model.encode(image="../../data/C3/imgs/datawhale01.png", text=text)
     img_emb_2 = model.encode(image="../../data/C3/imgs/datawhale02.png")
-    multi_emb_2 = model.encode(image="../../data/C3/imgs/datawhale02.png", text="datawhale开源组织的logo")
+    multi_emb_2 = model.encode(image="../../data/C3/imgs/datawhale02.png", text=text)
 
 # 计算相似度
 sim_1 = img_emb_1 @ img_emb_2.T
